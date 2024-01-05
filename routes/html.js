ac9fea44ce629a483d17d4866cd4097e92711e59
Jess Skelton <jessskelton888@gmail.com>
2024-01-05 15:50:38 -0600

    Added code to html.js for get requests to show index.html & notes.html

--- routes/html.js
+++ routes/html.js
@@ -1,9 +1,17 @@
 const htmlRoute = require('express').Router();
 const path = require('path');
 
-// GET request responding with index.js
+// GET request responding with index.js -- Should return index.html even if user attempts to visit routes that don't exist
+// POSSIBLE DEBUG -- if broken, change * to /
+htmlRoute.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 // GET request responding with notes.html
+htmlRoute.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-// -------------------------------------
+
+
 module.exports = htmlRoute;
