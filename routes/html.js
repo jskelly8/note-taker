047e6205eae234d3a26235d2c5d988659796d33a
Jess Skelton <jessskelton888@gmail.com>
2024-01-05 15:39:19 -0600

    Added content to server.js beginning the routing to newly created api.js and html.js inside new routes folder & Added/Installed UUID to package.json

---
+++ routes/html.js
@@ -0,0 +1,9 @@
+const htmlRoute = require('express').Router();
+const path = require('path');
+
+// GET request responding with index.js
+
+// GET request responding with notes.html
+
+// -------------------------------------
+module.exports = htmlRoute;
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
+const apiRoute = require('./routes/api.js');
+const htmlRoute = require('./routes/html.js');
 const app = express();
-const PORT = process.env.PORT || 3001; // You can choose a different port
+const PORT = process.env.PORT || 3001; 
 
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(apiRoute);
+app.use(htmlRoute);
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on local host port ${PORT}`);
+});
+
+// -------------------------------
 // Serve static files from a public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -23,7 +39,3 @@ app.get('/path/:variable', (req, res) => {
   res.send(`Variable value: ${variable}`);
 });
 
-// Start the server and listen on the specified port
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
