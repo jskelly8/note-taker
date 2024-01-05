412dd1f90ca59c667292fbfe14bc9ebb1c9bc62e
Jess Skelton <jessskelton888@gmail.com>
2024-01-05 17:29:50 -0600

    Updated get request to be universal on main.js, & updated placement of main.js vs notes.js on server.js for load

--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,14 @@
 const mainRoute = require('express').Router();
 const path = require('path');
 
-// GET request responding with index.js -- Should return index.html even if user attempts to visit routes that don't exist
-mainRoute.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 // GET request responding with notes.html
 mainRoute.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// GET request responding with index.js -- Should return index.html even if user attempts to visit routes that don't exist
+mainRoute.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const notesRoute = require('./routes/notes.js');
 const mainRoute = require('./routes/main.js');
+const notesRoute = require('./routes/notes.js');
 const app = express();
 const PORT = process.env.PORT || 3001; 
 
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(notesRoute);
 app.use(mainRoute);
+app.use(notesRoute);
 
 // Starts the server and listen on the specified port
 app.listen(PORT, () => {
